0637a543ef3a5b7183372a374ac363e94bca7483
trimbe <benno.tremback@gmail.com>
2018-12-21 13:19:00 -0500

    retrieve ItemLayer height in render rather than on spawn

--- runelite-client/src/main/java/net/runelite/client/plugins/grounditems/GroundItemsPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/grounditems/GroundItemsPlugin.java
@@ -378,7 +378,7 @@ public class GroundItemsPlugin extends Plugin
 			.quantity(item.getQuantity())
 			.name(itemComposition.getName())
 			.haPrice(alchPrice)
-			.height(tile.getItemLayer().getHeight())
+			.height(-1)
 			.tradeable(itemComposition.isTradeable())
 			.build();
 
