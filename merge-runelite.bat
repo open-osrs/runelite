c26ed0e353af0008a0caff50d33423f34c7f16fa
ThatGamerBlue <thatgamerblue@gmail.com>
2019-04-25 10:16:00 +0000

    clues: Fix Easy Cryptic in Kourend Centre Turns out they changed the text for this one

---
+++ merge-runelite.bat
@@ -0,0 +1,25 @@
+@echo off
+
+git checkout runelite
+
+git pull https://github.com/runelite/runelite.git master
+
+echo Please check for merge conflicts now
+
+echo Please check for merge conflicts now
+
+echo Please check for merge conflicts now
+
+echo Please check for merge conflicts now
+
+echo Please check for merge conflicts now
+
+pause
+
+git add .
+
+git commit -m "Merge runelite/runelite master"
+
+git push origin runelite
+
+pause
