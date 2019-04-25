efd062f1b26b4d1e52056cab4c472ca2882f7219
PKLite <stonewall@pklite.xyz>
2019-04-25 10:54:43 -0400

    fixes 1-way safe spots to no longer indicate unreachable tiles
    Signed-off-by: PKLite <stonewall@pklite.xyz>

--- runelite-client/src/main/java/net/runelite/client/plugins/safespot/SafeSpotConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/safespot/SafeSpotConfig.java
@@ -1,3 +1,12 @@
+/*
+ *
+ *  Copyright (c) 2019. PKLite
+ *   Redistributions and modifications of this software are permitted as long as this notice remains in its original unmodified state at the top of this file. If there are any questions, comments, or feedback about this software, please direct all inquiries directly to the following authors.
+ *   PKLite discord: https://discord.gg/Dp3HuFM
+ *  Written by PKLite(ST0NEWALL, others) <stonewall@stonewall@pklite.xyz>, 2019
+ *
+ */
+
 package net.runelite.client.plugins.safespot;
 
 import java.awt.Color;
