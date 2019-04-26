fa88afa9a3cad547ac3f6aeb319cb9b85b860f64
pklite <stonewall@pklite.xyz>
2019-04-26 00:17:51 +0000

    fixing this one as well

--- runelite-client/src/main/java/net/runelite/client/plugins/learntoclick/LearnToClickPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/learntoclick/LearnToClickPlugin.java
@@ -11,6 +11,7 @@
 package net.runelite.client.plugins.learntoclick;
 
 import com.google.inject.Provides;
+import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.api.MenuEntry;
@@ -22,76 +23,80 @@ import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import org.apache.commons.lang3.ArrayUtils;
 
-import javax.inject.Inject;
-
 
 @PluginDescriptor(
-        name = "Learn to Click",
-        description = "A few modifications to prevent misclicks",
-        tags = {"pklite","pk","misclick","compass","map","auto retaliate"},
-        enabledByDefault = false
+	name = "Learn to Click",
+	description = "A few modifications to prevent misclicks",
+	tags = {"pklite", "pk", "misclick", "compass", "map", "auto retaliate"},
+	enabledByDefault = false
 )
 @Slf4j
 public class LearnToClickPlugin extends Plugin
 {
-    @Inject
-    private LearnToClickConfig config;
-    private boolean forceRightClickFlag;
-    private MenuEntry[] entries;
-    @Inject
-    private Client client;
+	@Inject
+	private LearnToClickConfig config;
+	private boolean forceRightClickFlag;
+	private MenuEntry[] entries;
+	@Inject
+	private Client client;
 
-    @Provides
-    LearnToClickConfig provideConfig(ConfigManager configManager) { return configManager.getConfig(LearnToClickConfig.class); }
+	@Provides
+	LearnToClickConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(LearnToClickConfig.class);
+	}
 
-    @Override
-    protected void startUp() throws Exception
-    {
+	@Override
+	protected void startUp() throws Exception
+	{
 
-    }
+	}
 
-    @Override
-    protected void shutDown() throws Exception
-    {
-        forceRightClickFlag = false;
-    }
+	@Override
+	protected void shutDown() throws Exception
+	{
+		forceRightClickFlag = false;
+	}
 
-    @Subscribe
-    public void onMenuShouldLeftClick(MenuShouldLeftClick event)
-    {
-        if (!forceRightClickFlag)
-        {
-            return;
-        }
-        forceRightClickFlag = false;
-        MenuEntry[] menuEntries = client.getMenuEntries();
-        for (MenuEntry entry : menuEntries)
-        {
-            if ((entry.getOption().equals("Floating") && config.shouldRightClickMap()) || (entry.getOption().equals("Hide") && config.shouldRightClickXp()) || (entry.getOption().equals("Show") && config.shouldRightClickXp()) || (entry.getOption().equals("Auto retaliate") && config.shouldRightClickRetaliate()))
-            {
-                event.setForceRightClick(true);
-                return;
-            }
-        }
-    }
+	@Subscribe
+	public void onMenuShouldLeftClick(MenuShouldLeftClick event)
+	{
+		if (!forceRightClickFlag)
+		{
+			return;
+		}
+		forceRightClickFlag = false;
+		MenuEntry[] menuEntries = client.getMenuEntries();
+		for (MenuEntry entry : menuEntries)
+		{
+			if ((entry.getOption().equals("Floating") && config.shouldRightClickMap()) || (entry.getOption().equals("Hide") && config.shouldRightClickXp()) || (entry.getOption().equals("Show") && config.shouldRightClickXp()) || (entry.getOption().equals("Auto retaliate") && config.shouldRightClickRetaliate()))
+			{
+				event.setForceRightClick(true);
+				return;
+			}
+		}
+	}
 
-    @Subscribe
-    public void onMenuEntryAdded(MenuEntryAdded event)
-    {
-        if ((event.getOption().equals("Floating") && config.shouldRightClickMap()) || (event.getOption().equals("Hide") && config.shouldRightClickXp()) || (event.getOption().equals("Show") && config.shouldRightClickXp()) || (event.getOption().equals("Auto retaliate") && config.shouldRightClickRetaliate()))
-        {
-            forceRightClickFlag = true;
-        }
-        entries = client.getMenuEntries();
-        if (config.shouldBlockCompass()) {
-            for (int i = entries.length - 1; i >= 0; i--) {
-                if (entries[i].getOption().equals("Look North")) {
-                    entries = ArrayUtils.remove(entries, i);
-                    i--;
-                }
-            }
-            client.setMenuEntries(entries);
-        }
+	@Subscribe
+	public void onMenuEntryAdded(MenuEntryAdded event)
+	{
+		if ((event.getOption().equals("Floating") && config.shouldRightClickMap()) || (event.getOption().equals("Hide") && config.shouldRightClickXp()) || (event.getOption().equals("Show") && config.shouldRightClickXp()) || (event.getOption().equals("Auto retaliate") && config.shouldRightClickRetaliate()))
+		{
+			forceRightClickFlag = true;
+		}
+		entries = client.getMenuEntries();
+		if (config.shouldBlockCompass())
+		{
+			for (int i = entries.length - 1; i >= 0; i--)
+			{
+				if (entries[i].getOption().equals("Look North"))
+				{
+					entries = ArrayUtils.remove(entries, i);
+					i--;
+				}
+			}
+			client.setMenuEntries(entries);
+		}
 
-    }
+	}
 }
