67a61f9b42c4fd06e3d8c2d2e2d9cb42749a684c
Jonathan <48493132+Raieverr@users.noreply.github.com>
2019-05-10 18:07:27 -0600

    Updated Hide Prayers Plugin
    -Issue #219 Fixed
    -Split Prayer Swap for PVP and PVM
    -Added in full PVP suite prayer customization
    -Added in custom PVM prayer sets for individual bosses

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/hideprayers/PVM/HidePrayersPVMConfig.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2018, https://runelitepl.us
+ * Copyright (c) 2019, Kyle <https://github.com/kyleeld>
+ * Copyright (c) 2018, Raiever <https://github.com/Raieverr>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package net.runelite.client.plugins.hideprayers.PVM;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+@ConfigGroup("hideprayersPVM")
+public interface HidePrayersPVMConfig extends Config
+{
+	@ConfigItem
+	(
+		position = 0,
+		keyName = "barrowscheap",
+		name = "Barrows Eagle Eye/Mystic Might",
+		description = "Shows prayers for Barrows"
+	)
+	default boolean showBarrowsCheap()
+	{
+		return false;
+	}
+
+	@ConfigItem
+	(
+		position = 1,
+		keyName = "barrowsexpensive",
+		name = "Barrows Augury/Rigour",
+		description = "Shows prayers for Barrows"
+	)
+	default boolean showBarrowsExpensive()
+	{
+		return false;
+	}
+
+	@ConfigItem
+	(
+		position = 2,
+		keyName = "zulrahcheap",
+		name = "Zulrah Eagle Eye/Mystic Might",
+		description = "Hide/Show Eagle Eye/Mystic Might for Zulrah"
+	)
+	default boolean showZulrahCheap()
+	{
+		return false;
+	}
+
+	@ConfigItem
+	(
+		position = 3,
+		keyName = "zulrahexpensive",
+		name = "Zulrah Augury/Rigour",
+		description = "Shows prayers needed for Zulrah"
+	)
+	default boolean showZulrahExpensive()
+	{
+		return false;
+	}
+
+}
