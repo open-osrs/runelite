7821854895d79a018836793793c90401cf5f0555
xperiaclash <friggle456@gmail.com>
2019-05-25 21:43:14 +0200

    Modified stuff

--- runelite-client/src/main/java/net/runelite/client/plugins/banlist/BanListPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/banlist/BanListPlugin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Hydrox6 <ikada@protonmail.ch>
+ * Copyright (c) 2019 xperiaclash <https://github.com/xperiaclash>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -24,13 +24,17 @@
  */
 package net.runelite.client.plugins.banlist;
 
-
 import com.google.inject.Provides;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import javax.inject.Inject;
+import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.ClanMember;
 import net.runelite.api.Client;
-import net.runelite.api.events.ClanChanged;
 import net.runelite.api.events.ClanMemberJoined;
+import net.runelite.api.events.ConfigChanged;
 import net.runelite.client.chat.ChatColorType;
 import net.runelite.client.chat.ChatMessageBuilder;
 import net.runelite.client.chat.ChatMessageManager;
@@ -46,19 +50,13 @@ import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Request;
 import okhttp3.Response;
-import javax.inject.Inject;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
 
 @PluginDescriptor(
-	name = "Ban List",
-	description = "Displays warning in chat when you join a clan chat/new member join your clan chat and he is in a WDR/RuneWatch List",
-	tags = {"PVM","WDR","RuneWatch"},
-	type = PluginType.UTILITY
-
+		name = "Ban List",
+		description = "Displays warning in chat when you join a" +
+				"clan chat/new member join your clan chat and he is in a WDR/RuneWatch List",
+		tags = {"PVM", "WDR", "RuneWatch"},
+		type = PluginType.UTILITY
 )
 public class BanListPlugin extends Plugin
 {
@@ -71,82 +69,85 @@ public class BanListPlugin extends Plugin
 	@Inject
 	private ChatMessageManager chatMessageManager;
 
+	private ArrayList<String> wdrArrayList = new ArrayList<>();
+	private ArrayList<String> runeWatchArrayList = new ArrayList<>();
+	private ArrayList<String> manualBans = new ArrayList<>();
+
 	@Provides
-    BanListConfig getConfig(ConfigManager configManager)
+	BanListConfig getConfig(ConfigManager configManager)
 	{
 		return configManager.getConfig(BanListConfig.class);
 	}
 
-	private ArrayList<String> wdrArrayList = new ArrayList<>();
-	private ArrayList<String> runeWatchArrayList = new ArrayList<>();
-
-
-
-
-
 	@Override
-	protected void shutDown() throws Exception {
-		wdrArrayList = null;
-		runeWatchArrayList = null;
-
-	}
-
-	@Override
-	protected void startUp() throws Exception {
+	protected void startUp() throws Exception
+	{
 		wdrArrayList = new ArrayList<>();
 		runeWatchArrayList = new ArrayList<>();
-
+		manualBans.addAll(Text.fromCSV(config.getBannedPlayers()));
 		fetchFromWebsites();
-
 	}
 
-
-
+	@Override
+	protected void shutDown() throws Exception
+	{
+		wdrArrayList = null;
+		runeWatchArrayList = null;
+		manualBans = null;
+	}
 
 	@Subscribe
 	public void onClanMemberJoined(ClanMemberJoined event)
 	{
-		// this event is fired when someone joins your cc, if you however join a cc, it will fire for every single member in your cc
 		ClanMember member = event.getMember();
+		checkBanList(Text.standardize(member.getUsername()));
+	}
 
-		checkBanList(Text.standardize(member.getUsername())); //
-
+	@Subscribe
+	public void onConfigChanged(ConfigChanged event)
+	{
+		for (String manual : Text.fromCSV(config.getBannedPlayers()))
+		{
+			if (!manualBans.contains(manual))
+			{
+				manualBans.add(manual);
+			}
+		}
 	}
 
 	private void checkBanList(String nameToBeChecked)
 	{
-
-		if(client.getClanMembers() != null )
+		if (client.getClanMembers() != null)
 		{
+			if (wdrArrayList.size() > 1 && config.enableWDR())
+			{
+				if (wdrArrayList.stream().anyMatch(nameToBeChecked::equalsIgnoreCase))
+				{
+					sendWarning(nameToBeChecked, 1);
+				}
+			}
 
-					if(wdrArrayList.size()>1 && config.enableWDR()){
+			if (runeWatchArrayList.size() > 1 && config.enableRuneWatch())
+			{
+				if (runeWatchArrayList.stream().anyMatch(nameToBeChecked::equalsIgnoreCase))
+				{
+					sendWarning(nameToBeChecked, 2);
+				}
+			}
 
-						if(wdrArrayList.stream().anyMatch(nameToBeChecked::equalsIgnoreCase))
-						{
-							sendWarning(nameToBeChecked,1);
-
-
-						}
-
-					}
-
-					if(runeWatchArrayList.size()>1 && config.enableRuneWatch()) {
-
-						if (runeWatchArrayList.stream().anyMatch(nameToBeChecked::equalsIgnoreCase)) {
-							sendWarning(nameToBeChecked, 2);
-						}
-
-
-					}
-
+			if (manualBans.size() > 1)
+			{
+				if (manualBans.stream().anyMatch(nameToBeChecked::equalsIgnoreCase))
+				{
+					sendWarning(nameToBeChecked, 3);
+				}
+			}
 		}
-
-
 	}
 
 	private void sendWarning(String playerName, int list)
 	{
-		if(list == 1)
+		if (list == 1)
 		{
 			// on wdr list
 			final String message = new ChatMessageBuilder()
@@ -159,10 +160,8 @@ public class BanListPlugin extends Plugin
 							.type(ChatMessageType.CONSOLE)
 							.runeLiteFormattedMessage(message)
 							.build());
-
-
 		}
-		else if(list == 2)
+		else if (list == 2)
 		{
 			// on runewatch list
 			final String message = new ChatMessageBuilder()
@@ -175,11 +174,22 @@ public class BanListPlugin extends Plugin
 							.type(ChatMessageType.CONSOLE)
 							.runeLiteFormattedMessage(message)
 							.build());
-
 		}
+		else if (list == 3)
+		{
+			// on manual list
+			final String message = new ChatMessageBuilder()
+					.append(ChatColorType.HIGHLIGHT)
+					.append("Warning! " + playerName + " is on Your Manual ban List")
+					.build();
 
+			chatMessageManager.queue(
+					QueuedMessage.builder()
+							.type(ChatMessageType.CONSOLE)
+							.runeLiteFormattedMessage(message)
+							.build());
+		}
 	}
-
 
 
 	private void fetchFromWebsites()
@@ -187,25 +197,27 @@ public class BanListPlugin extends Plugin
 		Request request = new Request.Builder()
 				.url("https://wdrdev.github.io/index")
 				.build();
-		RuneLiteAPI.CLIENT.newCall(request).enqueue(new Callback() {
+		RuneLiteAPI.CLIENT.newCall(request).enqueue(new Callback()
+		{
 			@Override
-			public void onFailure(Call call, IOException e) {
+			public void onFailure(Call call, IOException e)
+			{
 				log.println("error retrieving names from wdr");
 			}
 
 			@Override
-			public void onResponse(Call call, Response response) throws IOException {
+			public void onResponse(Call call, Response response) throws IOException
+			{
 				String text = response.body().string();
-				text = text.substring(text.indexOf("<p>")+3,text.indexOf("</p>"));
-				text = text.replace("/",",");
+				text = text.substring(text.indexOf("<p>") + 3, text.indexOf("</p>"));
+				text = text.replace("/", ",");
 				text = text.replace(", $", "");
 
 				ArrayList<String> wdrList = new ArrayList<>(Arrays.asList(text.split(",")));
 				ArrayList<String> wdrList2 = new ArrayList<>();
-				wdrList.forEach( (name) -> wdrList2.add(Text.standardize(name)));
+				wdrList.forEach((name) -> wdrList2.add(Text.standardize(name)));
 
 				wdrArrayList.addAll(wdrList2);
-
 			}
 		});
 
@@ -213,46 +225,30 @@ public class BanListPlugin extends Plugin
 		Request secondRequest = new Request.Builder()
 				.url("https://runewatch.com/incident-index-page/")
 				.build();
-		RuneLiteAPI.CLIENT.newCall(secondRequest).enqueue(new Callback() {
+		RuneLiteAPI.CLIENT.newCall(secondRequest).enqueue(new Callback()
+		{
 			@Override
-			public void onFailure(Call call, IOException e) {
+			public void onFailure(Call call, IOException e)
+			{
 				log.println("error retrieving names from runewatch");
 			}
 
 			@Override
-			public void onResponse(Call call, Response response) throws IOException {
+			public void onResponse(Call call, Response response) throws IOException
+			{
 				String text = response.body().string();
-				String mytext = text.substring(text.indexOf("lcp_instance_0"),text.indexOf("strong>Evidence Quality Suggestion"));
-				mytext.replace("</a>","");
-				mytext.replace("<div class=\"su-tabs-pane su-clearfix\">","");
-				mytext.replace(", $", "");
+				String mytext = text.substring(text.indexOf("lcp_instance_0"), text.indexOf("strong>Evidence Quality Suggestion"));
 				String[] split = mytext.split("href=");
-				for(String x: split)
+				for (String x : split)
 				{
-
-					if(x.contains("title")){
-						x = x.substring(x.indexOf("title"),x.indexOf(">"));
-						x = x.substring(x.indexOf("=")+2,x.length()-1);
+					if (x.contains("title"))
+					{
+						x = x.substring(x.indexOf("title"), x.indexOf(">"));
+						x = x.substring(x.indexOf("=") + 2, x.length() - 1);
 						runeWatchArrayList.add(Text.standardize(x));
-
 					}
-
-
 				}
-
-
 			}
 		});
-
-
 	}
-
-
-
-
-
-
-
-
-
 }
