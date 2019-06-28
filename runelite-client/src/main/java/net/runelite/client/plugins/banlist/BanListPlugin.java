8ba3fc03d8f6b2391dc32c0ee2e9afb55e8c941c
sdburns1998 <sdburn1998@gmail.com>
2019-06-28 22:55:03 +0200

    Save config values -- BanListPlugin

--- runelite-client/src/main/java/net/runelite/client/plugins/banlist/BanListPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/banlist/BanListPlugin.java
@@ -94,11 +94,18 @@ public class BanListPlugin extends Plugin
 		return configManager.getConfig(BanListConfig.class);
 	}
 
+	// save config values
+	private boolean enableWDR;
+	private boolean enableRuneWatch;
+	private boolean highlightInClan;
+	private boolean highlightInTrade;
+
 	@Override
 	protected void startUp() throws Exception
 	{
 		manualBans.addAll(Text.fromCSV(config.getBannedPlayers()));
 		fetchFromWebsites();
+		updateConfig();
 	}
 
 	@Override
@@ -128,6 +135,14 @@ public class BanListPlugin extends Plugin
 		}
 	}
 
+	public void updateConfig()
+	{
+		this.enableWDR = config.enableWDR();
+		this.enableRuneWatch = config.enableRuneWatch();
+		this.highlightInClan = config.highlightInClan();
+		this.highlightInTrade = config.highlightInTrade();
+	}
+
 	/**
 	 * Event to keep making sure player names are highlighted red in clan chat, since the red name goes away frequently
 	 */
@@ -138,7 +153,7 @@ public class BanListPlugin extends Plugin
 			|| client.getWidget(WidgetInfo.LOGIN_CLICK_TO_PLAY_SCREEN) != null
 			|| client.getViewportWidget() == null
 			|| client.getWidget(WidgetInfo.CLAN_CHAT) == null
-			|| !config.highlightInClan())
+			|| !this.highlightInClan)
 		{
 			return;
 		}
@@ -163,7 +178,7 @@ public class BanListPlugin extends Plugin
 		if (scamList != null)
 		{
 			sendWarning(Text.standardize(member.getUsername()), scamList);
-			if (config.highlightInClan())
+			if (this.highlightInClan)
 			{
 				highlightRedInCC();
 			}
@@ -172,7 +187,7 @@ public class BanListPlugin extends Plugin
 		if (toxicList != null)
 		{
 			sendWarning(Text.standardize(member.getUsername()), toxicList);
-			if (config.highlightInClan())
+			if (this.highlightInClan)
 			{
 				highlightRedInCC();
 			}
@@ -185,7 +200,7 @@ public class BanListPlugin extends Plugin
 	@Subscribe
 	public void onWidgetLoaded(WidgetLoaded widgetLoaded)
 	{
-		if (config.highlightInTrade())
+		if (this.highlightInTrade)
 		{
 			if (widgetLoaded.getGroupId() == 335)
 			{ //if trading window was loaded
@@ -211,7 +226,7 @@ public class BanListPlugin extends Plugin
 	 */
 	private ListType checkScamList(String nameToBeChecked)
 	{
-		if (wdrScamArrayList.size() > 0 && config.enableWDR())
+		if (wdrScamArrayList.size() > 0 && this.enableWDR)
 		{
 			if (wdrScamArrayList.stream().anyMatch(nameToBeChecked::equalsIgnoreCase))
 			{
@@ -219,7 +234,7 @@ public class BanListPlugin extends Plugin
 			}
 		}
 
-		if (runeWatchArrayList.size() > 0 && config.enableRuneWatch())
+		if (runeWatchArrayList.size() > 0 && this.enableRuneWatch)
 		{
 			if (runeWatchArrayList.stream().anyMatch(nameToBeChecked::equalsIgnoreCase))
 			{
@@ -241,7 +256,7 @@ public class BanListPlugin extends Plugin
 	private ListType checkToxicList(String nameToBeChecked)
 	{
 
-		if (wdrToxicArrayList.size() > 0 && config.enableWDR())
+		if (wdrToxicArrayList.size() > 0 && this.enableWDR)
 		{
 			if (wdrToxicArrayList.stream().anyMatch(nameToBeChecked::equalsIgnoreCase))
 			{
