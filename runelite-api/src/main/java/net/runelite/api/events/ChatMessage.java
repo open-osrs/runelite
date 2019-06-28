abf7fec004148897585bc0389bb5e0420e3d03cb
sdburns1998 <sdburn1998@gmail.com>
2019-06-28 20:06:58 +0200

    Fix checkstyle

--- runelite-api/src/main/java/net/runelite/api/events/ChatMessage.java
+++ runelite-api/src/main/java/net/runelite/api/events/ChatMessage.java
@@ -28,7 +28,6 @@ import net.runelite.api.ChatMessageType;
 import net.runelite.api.MessageNode;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 /**
  * An event where a new chat message is received.
