63bb31670153b28ef00a6b50f00b45dc1d73a73e
sdburns1998 <sdburn1998@gmail.com>
2019-07-01 20:29:10 +0200

    Checkstyle

--- runelite-client/src/main/java/net/runelite/client/plugins/combatcounter/CombatOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/combatcounter/CombatOverlay.java
@@ -26,7 +26,6 @@ package net.runelite.client.plugins.combatcounter;
 
 import java.awt.Dimension;
 import java.awt.Graphics2D;
-import java.util.HashMap;
 import java.util.Map;
 import javax.inject.Inject;
 import net.runelite.api.Client;
