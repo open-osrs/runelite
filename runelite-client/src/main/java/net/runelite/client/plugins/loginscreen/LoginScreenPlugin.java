5cafab22dbee83372c667aad648a460244f064d7
sdburns1998 <sdburn1998@gmail.com>
2019-07-02 13:07:38 +0200

    Save config values -- LoginScreenPlugin

--- runelite-client/src/main/java/net/runelite/client/plugins/loginscreen/LoginScreenPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/loginscreen/LoginScreenPlugin.java
@@ -35,6 +35,7 @@ import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
+import net.runelite.api.events.ConfigChanged;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.events.SessionOpen;
 import net.runelite.client.config.ConfigManager;
@@ -67,9 +68,14 @@ public class LoginScreenPlugin extends Plugin implements KeyListener
 
 	private String usernameCache;
 
+	private boolean syncUsername;
+	private boolean pasteEnabled;
+	private String username;
+
 	@Override
 	protected void startUp() throws Exception
 	{
+		updateConfig();
 		applyUsername();
 		keyManager.registerKeyListener(this);
 	}
@@ -77,7 +83,7 @@ public class LoginScreenPlugin extends Plugin implements KeyListener
 	@Override
 	protected void shutDown() throws Exception
 	{
-		if (config.syncUsername())
+		if (this.syncUsername)
 		{
 			client.getPreferences().setRememberedUsername(usernameCache);
 		}
@@ -94,7 +100,7 @@ public class LoginScreenPlugin extends Plugin implements KeyListener
 	@Subscribe
 	public void onGameStateChanged(GameStateChanged event)
 	{
-		if (!config.syncUsername())
+		if (!this.syncUsername)
 		{
 			return;
 		}
@@ -112,13 +118,14 @@ public class LoginScreenPlugin extends Plugin implements KeyListener
 				username = client.getUsername();
 			}
 
-			if (config.username().equals(username))
+			if (this.username.equals(username))
 			{
 				return;
 			}
 
 			log.debug("Saving username: {}", username);
 			config.username(username);
+			this.username = username;
 		}
 	}
 
@@ -131,7 +138,7 @@ public class LoginScreenPlugin extends Plugin implements KeyListener
 
 	private void applyUsername()
 	{
-		if (!config.syncUsername())
+		if (!this.syncUsername)
 		{
 			return;
 		}
@@ -139,7 +146,7 @@ public class LoginScreenPlugin extends Plugin implements KeyListener
 		GameState gameState = client.getGameState();
 		if (gameState == GameState.LOGIN_SCREEN)
 		{
-			String username = config.username();
+			String username = this.username;
 
 			if (Strings.isNullOrEmpty(username))
 			{
@@ -164,7 +171,7 @@ public class LoginScreenPlugin extends Plugin implements KeyListener
 	@Override
 	public void keyPressed(KeyEvent e)
 	{
-		if (!config.pasteEnabled() || (
+		if (!this.pasteEnabled || (
 			client.getGameState() != GameState.LOGIN_SCREEN &&
 				client.getGameState() != GameState.LOGIN_SCREEN_AUTHENTICATOR))
 		{
@@ -219,5 +226,23 @@ public class LoginScreenPlugin extends Plugin implements KeyListener
 	public void keyReleased(KeyEvent e)
 	{
 
+	}
+
+	@Subscribe
+	public void onConfigChanged(ConfigChanged event)
+	{
+		if (!event.getGroup().equals("loginscreen"))
+		{
+			return;
+		}
+
+		updateConfig();
+	}
+
+	private void updateConfig()
+	{
+		this.syncUsername = config.syncUsername();
+		this.pasteEnabled = config.pasteEnabled();
+		this.username = config.username();
 	}
 }
