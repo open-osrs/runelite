db3346e060a5058f3abe27fe55563df300b1bc13
sdburns1998 <sdburn1998@gmail.com>
2019-07-05 17:50:00 +0200

    Add a button to the info panel to open the log dir (and fix the discord invite link)

--- runelite-client/src/main/java/net/runelite/client/plugins/info/InfoPanel.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/info/InfoPanel.java
@@ -30,10 +30,13 @@ import com.google.inject.Inject;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.Desktop;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.ScheduledExecutorService;
 import javax.annotation.Nullable;
 import javax.inject.Singleton;
@@ -45,6 +48,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.HyperlinkEvent;
 import net.runelite.api.Client;
+import static net.runelite.client.RuneLite.LOGS_DIR;
 import net.runelite.client.RuneLiteProperties;
 import net.runelite.client.account.SessionManager;
 import net.runelite.client.config.ConfigManager;
@@ -65,9 +69,9 @@ public class InfoPanel extends PluginPanel
 
 	private static final ImageIcon ARROW_RIGHT_ICON;
 	private static final ImageIcon GITHUB_ICON;
+	private static final ImageIcon FOLDER_ICON;
 	private static final ImageIcon DISCORD_ICON;
 	private static final ImageIcon PATREON_ICON;
-	private static final ImageIcon WIKI_ICON;
 	private static final ImageIcon IMPORT_ICON;
 
 	private final JLabel loggedLabel = new JLabel();
@@ -98,9 +102,9 @@ public class InfoPanel extends PluginPanel
 	{
 		ARROW_RIGHT_ICON = new ImageIcon(ImageUtil.getResourceStreamFromClass(InfoPanel.class, "/util/arrow_right.png"));
 		GITHUB_ICON = new ImageIcon(ImageUtil.getResourceStreamFromClass(InfoPanel.class, "github_icon.png"));
+		FOLDER_ICON = new ImageIcon(ImageUtil.getResourceStreamFromClass(InfoPanel.class, "folder_icon.png"));
 		DISCORD_ICON = new ImageIcon(ImageUtil.getResourceStreamFromClass(InfoPanel.class, "discord_icon.png"));
 		PATREON_ICON = new ImageIcon(ImageUtil.getResourceStreamFromClass(InfoPanel.class, "patreon_icon.png"));
-		WIKI_ICON = new ImageIcon(ImageUtil.getResourceStreamFromClass(InfoPanel.class, "wiki_icon.png"));
 		IMPORT_ICON = new ImageIcon(ImageUtil.getResourceStreamFromClass(InfoPanel.class, "import_icon.png"));
 	}
 
@@ -113,7 +117,7 @@ public class InfoPanel extends PluginPanel
 		JPanel versionPanel = new JPanel();
 		versionPanel.setBackground(ColorScheme.DARKER_GRAY_COLOR);
 		versionPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
-		versionPanel.setLayout(new GridLayout(0, 1));
+		versionPanel.setLayout(new GridLayout(4, 1));
 
 		final Font smallFont = FontManager.getSmallFont(getFont());
 
@@ -161,7 +165,7 @@ public class InfoPanel extends PluginPanel
 
 		actionsContainer = new JPanel();
 		actionsContainer.setBorder(new EmptyBorder(10, 0, 0, 0));
-		actionsContainer.setLayout(new GridLayout(0, 1, 0, 10));
+		actionsContainer.setLayout(new GridLayout(5, 1, 0, 10));
 
 		syncPanel = buildLinkPanel(IMPORT_ICON, "Import local settings", "to remote RuneLite account", () ->
 		{
@@ -177,7 +181,8 @@ public class InfoPanel extends PluginPanel
 		});
 
 		actionsContainer.add(buildLinkPanel(GITHUB_ICON, "License info", "for distribution", "https://github.com/runelite-extended/runelite/blob/master/LICENSE"));
-		actionsContainer.add(buildLinkPanel(DISCORD_ICON, "Talk to us on our", "discord server", "https://discord.gg/s2fzu5U"));
+		actionsContainer.add(buildLinkPanel(FOLDER_ICON, "Open logs directory", "(for bug reports)", LOGS_DIR));
+		actionsContainer.add(buildLinkPanel(DISCORD_ICON, "Talk to us on our", "discord server", "https://discord.gg/HN5gf3m"));
 		actionsContainer.add(buildLinkPanel(PATREON_ICON, "Patreon to support", "the RuneLitePlus devs", runeLiteProperties.getPatreonLink()));
 		/*		actionsContainer.add(buildLinkPanel(WIKI_ICON, "Information about", "RuneLite and plugins", runeLiteProperties.getWikiLink()));*/
 
@@ -189,9 +194,25 @@ public class InfoPanel extends PluginPanel
 	}
 
 	/**
+	 * Builds a link panel with a given icon, text and directory to open.
+	 */
+	private static JPanel buildLinkPanel(ImageIcon icon, String topText, String bottomText, File dir)
+	{
+		return buildLinkPanel(icon, topText, bottomText, () -> {
+			try
+			{
+				Desktop.getDesktop().open(dir);
+			}
+			catch (IOException ex)
+			{
+			}
+		});
+	}
+
+	/**
 	 * Builds a link panel with a given icon, text and url to redirect to.
 	 */
-	private JPanel buildLinkPanel(ImageIcon icon, String topText, String bottomText, String url)
+	private static JPanel buildLinkPanel(ImageIcon icon, String topText, String bottomText, String url)
 	{
 		return buildLinkPanel(icon, topText, bottomText, () -> LinkBrowser.browse(url));
 	}
@@ -199,7 +220,7 @@ public class InfoPanel extends PluginPanel
 	/**
 	 * Builds a link panel with a given icon, text and callable to call.
 	 */
-	private JPanel buildLinkPanel(ImageIcon icon, String topText, String bottomText, Runnable callback)
+	private static JPanel buildLinkPanel(ImageIcon icon, String topText, String bottomText, Runnable callback)
 	{
 		JPanel container = new JPanel();
 		container.setBackground(ColorScheme.DARKER_GRAY_COLOR);
@@ -253,11 +274,11 @@ public class InfoPanel extends PluginPanel
 
 		JLabel topLine = new JLabel(topText);
 		topLine.setForeground(Color.WHITE);
-		topLine.setFont(FontManager.getSmallFont(getFont()));
+		topLine.setFont(FontManager.getRunescapeSmallFont());
 
 		JLabel bottomLine = new JLabel(bottomText);
 		bottomLine.setForeground(Color.WHITE);
-		bottomLine.setFont(FontManager.getSmallFont(getFont()));
+		bottomLine.setFont(FontManager.getRunescapeSmallFont());
 
 		textContainer.add(topLine);
 		textContainer.add(bottomLine);
