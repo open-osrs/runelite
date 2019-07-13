11d2ccf646bcbc9f0921de8d2133e3cc9c600bdf
Alexander Lohaus <alexander.lohaus@gmail.com>
2019-07-13 17:28:12 +0200

    vorkathhelper: import formatting

--- runelite-client/src/main/java/net/runelite/client/plugins/vorkath/AcidPathOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/vorkath/AcidPathOverlay.java
@@ -26,6 +26,10 @@
  */
 package net.runelite.client.plugins.vorkath;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
 import net.runelite.api.Client;
 import net.runelite.api.Perspective;
 import net.runelite.api.coords.LocalPoint;
@@ -35,7 +39,6 @@ import net.runelite.client.ui.overlay.OverlayLayer;
 import net.runelite.client.ui.overlay.OverlayPosition;
 import net.runelite.client.ui.overlay.OverlayUtil;
 import javax.inject.Inject;
-import java.awt.*;
 
 public class AcidPathOverlay extends Overlay
 {
