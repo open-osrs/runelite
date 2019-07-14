b72348ff617a76834620ec070ae4462b926a127a
Ganom <theariall@gmail.com>
2019-07-14 11:43:17 -0400

    Remove old imports.

--- http-api/src/main/java/net/runelite/http/api/discord/DiscordClient.java
+++ http-api/src/main/java/net/runelite/http/api/discord/DiscordClient.java
@@ -37,8 +37,6 @@ import okhttp3.MediaType;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Slf4j
 public class DiscordClient
