19e633c7c0c5c0298b5b2dbd47e06954940a8969
Kyleeld <48519776+Kyleeld@users.noreply.github.com>
2019-07-30 13:29:51 +0100

    Update GauntletOverlay.java

--- runelite-client/src/main/java/net/runelite/client/plugins/gauntlet/GauntletOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/gauntlet/GauntletOverlay.java
@@ -8,6 +8,13 @@
 
 package net.runelite.client.plugins.gauntlet;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.SystemColor;
 import java.util.List;
 import net.runelite.api.Client;
 import net.runelite.api.GameObject;
@@ -22,9 +29,7 @@ import net.runelite.client.ui.overlay.OverlayLayer;
 import net.runelite.client.ui.overlay.OverlayPosition;
 import net.runelite.client.ui.overlay.OverlayPriority;
 import net.runelite.client.ui.overlay.OverlayUtil;
-
 import javax.inject.Inject;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class GauntletOverlay extends Overlay
