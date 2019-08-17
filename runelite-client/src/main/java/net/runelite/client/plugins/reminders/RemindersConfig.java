c2a94d8d1c3631fca87e6e039f606c8ecd7317f4
Kyleeld <48519776+Kyleeld@users.noreply.github.com>
2019-08-17 03:27:55 +0100

    .

--- runelite-client/src/main/java/net/runelite/client/plugins/reminders/RemindersConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/reminders/RemindersConfig.java
@@ -1,3 +1,28 @@
+/*
+ * Copyright (c) 2019, RuneLitePlus <https://runelitepl.us>
+ * Copyright (c) 2019, kyle <https://github.com/Kyleeld>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package net.runelite.client.plugins.reminders;
 
 import net.runelite.client.config.Config;
@@ -10,23 +35,23 @@ public interface RemindersConfig extends Config
 	@ConfigItem(
 		keyName = "breakReminder",
 		name = "Break Reminder",
-		description = "Reminds you to take a break every hour",
+		description = "Reminds you to take a 5-10 minute break every hour.",
 		position = 0
 	)
 	default boolean breakReminder()
 	{
-		return true;
+		return false;
 	}
 
 	@ConfigItem(
 		keyName = "hydrationReminder",
 		name = "Hydration Reminder",
-		description = "Reminds you to drink water every hour",
+		description = "Reminds you to drink 4oz/120ml of water every hour",
 		position = 1
 	)
 	default boolean hydrationReminder()
 	{
-		return true;
+		return false;
 	}
 
 
--- runelite-client/src/main/java/net/runelite/client/plugins/reminders/RemindersPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/reminders/RemindersPlugin.java
@@ -1,3 +1,28 @@
+/*
+ * Copyright (c) 2019, jkybtw <https://github.com/jkybtw>
+ * Copyright (c) 2019, RuneLitePlus <https://runelitepl.us>
+ * Copyright (c) 2019, kyle <https://github.com/Kyleeld>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package net.runelite.client.plugins.reminders;
 
 import com.google.inject.Provides;
@@ -133,7 +158,6 @@ public class RemindersPlugin extends Plugin
 						final ChatMessageBuilder breakreminderhour = new ChatMessageBuilder()
 							.append(ChatColorType.NORMAL)
 							.append("You have hit the 1 hour mark, you should take a 5-10 minute break.");
-
 						chatMessageManager.queue(QueuedMessage.builder()
 							.type(ChatMessageType.FRIENDSCHATNOTIFICATION)
 							.runeLiteFormattedMessage(breakreminderhour.build())
@@ -147,7 +171,6 @@ public class RemindersPlugin extends Plugin
 						final ChatMessageBuilder breakreminderhours = new ChatMessageBuilder()
 							.append(ChatColorType.NORMAL)
 							.append("You have hit the " + hours + " hours mark, you should take a 5-10 minute break.");
-
 						chatMessageManager.queue(QueuedMessage.builder()
 							.type(ChatMessageType.FRIENDSCHATNOTIFICATION)
 							.runeLiteFormattedMessage(breakreminderhours.build())
@@ -181,7 +204,6 @@ public class RemindersPlugin extends Plugin
 						final ChatMessageBuilder hydrationreminderhours = new ChatMessageBuilder()
 							.append(ChatColorType.NORMAL)
 							.append("You have been logged in for " + hours + " hours.");
-
 						chatMessageManager.queue(QueuedMessage.builder()
 							.type(ChatMessageType.FRIENDSCHATNOTIFICATION)
 							.runeLiteFormattedMessage(hydrationreminderhours.build())
@@ -193,8 +215,6 @@ public class RemindersPlugin extends Plugin
 			final ChatMessageBuilder hydrationreminder = new ChatMessageBuilder()
 					.append(ChatColorType.NORMAL)
 					.append("By this point, you should have consumed at least " + ounces + "oz (" + millilitres + "ml) of Water to maintain optimum hydration.");
-
-
 				chatMessageManager.queue(QueuedMessage.builder()
 					.type(ChatMessageType.FRIENDSCHATNOTIFICATION)
 					.runeLiteFormattedMessage(hydrationreminder.build())
