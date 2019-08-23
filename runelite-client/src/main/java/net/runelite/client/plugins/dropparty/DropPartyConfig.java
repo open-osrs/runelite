ad62a1f637f56aa1b4347f279584f0751b692f87
Kyle <48519776+xKylee@users.noreply.github.com>
2019-08-23 10:45:09 +0100

    Update DropPartyConfig.java

--- runelite-client/src/main/java/net/runelite/client/plugins/dropparty/DropPartyConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/dropparty/DropPartyConfig.java
@@ -1,107 +1,18 @@
-/*
- * Copyright (c) 2017, Adam <Adam@sigterm.info>
- * All rights reserved.
- *
- *
- * Modified by farhan1666
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package net.runelite.client.plugins.dropparty;
 
+import java.awt.Color;
+import java.awt.Font;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 import net.runelite.client.config.Range;
-
-import java.awt.*;
+import net.runelite.client.config.Stub;
 
 @ConfigGroup("drop")
 public interface DropPartyConfig extends Config
 {
-	@ConfigItem(
-			keyName = "playerName",
-			name = "Dropping player:",
-			description = "selects what players name to mark tiles",
-			position = 0
-	)
-	default String playerName()
-	{
-		return "";
-	}
-
-	@Range(
-			min = 1,
-			max = 40
-	)
-	@ConfigItem(
-			keyName = "showAmmount",
-			name = "Trail length",
-			description = "Shows the legnth of the droppers trail",
-			position = 1
-	)
-	default int showAmmount()
-	{
-		return 10;
-	}
-
-	@ConfigItem(
-			position = 2,
-			keyName = "overlayColor",
-			name = "Overlay Color",
-			description = "Configures the color of the overlay"
-	)
-	default Color overlayColor()
-	{
-		return new Color(0, 150, 200);
-	}
-
-	@ConfigItem(
-			position = 3,
-			keyName = "fontStyle",
-			name = "Font Style",
-			description = "Bold/Italics/Plain"
-	)
-	default FontStyle fontStyle()
-	{
-		return FontStyle.BOLD;
-	}
-
-	@Range(
-			min = 10,
-			max = 40
-	)
-	@ConfigItem(
-			position = 4,
-			keyName = "textSize",
-			name = "Text Size",
-			description = "Text Size for Timers."
-	)
-	default int textSize()
-	{
-		return 18;
-	}
-
 	@Getter
 	@AllArgsConstructor
 	enum FontStyle
@@ -120,5 +31,66 @@ public interface DropPartyConfig extends Config
 		}
 	}
 
+	@ConfigItem(
+		keyName = "playerName",
+		name = "Dropping player:",
+		description = "selects what players name to mark tiles",
+		position = 0
+	)
+	default String playerName()
+	{
+		return "";
+	}
 
+	@Range(
+		min = 1,
+		max = 40
+	)
+	@ConfigItem(
+		keyName = "showAmmount",
+		name = "trail length",
+		description = "Shows the legnth of the droppers trail",
+		position = 1
+	)
+	default int showAmmount()
+	{
+		return 10;
+	}
+
+	@ConfigItem(
+		position = 2,
+		keyName = "overlayColor",
+		name = "Overlay Color",
+		description = "Configures the color of the overlay"
+	)
+	default Color overlayColor()
+	{
+		return new Color(0, 150, 200);
+	}
+
+	@ConfigItem(
+		position = 3,
+		keyName = "fontStyle",
+		name = "Font Style",
+		description = "Bold/Italics/Plain"
+	)
+	default FontStyle fontStyle()
+	{
+		return FontStyle.BOLD;
+	}
+
+	@Range(
+		min = 10,
+		max = 40
+	)
+	@ConfigItem(
+		position = 4,
+		keyName = "textSize",
+		name = "Text Size",
+		description = "Text Size for Timers."
+	)
+	default int textSize()
+	{
+		return 18;
+	}
 }
