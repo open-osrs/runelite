db7bfaa20ebf71a9a17eb5a5001925c852ba7582
lordzuku <47382776+lordzuku@users.noreply.github.com>
2019-08-23 05:41:48 -0400

    updated formatting
    fixed code conventions

--- runelite-client/src/main/java/net/runelite/client/plugins/dropparty/DropPartyConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/dropparty/DropPartyConfig.java
@@ -1,92 +1,124 @@
+/*
+ * Copyright (c) 2017, Adam <Adam@sigterm.info>
+ * All rights reserved.
+ *
+ *
+ * Modified by farhan1666
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package net.runelite.client.plugins.dropparty;
-import java.awt.Color;
-import java.awt.Font;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 import net.runelite.client.config.Range;
-import net.runelite.client.config.Stub;
+
+import java.awt.*;
 
 @ConfigGroup("drop")
-public interface DropPartyConfig extends Config {
-    @Getter
-    @AllArgsConstructor
-    enum FontStyle
-    {
-        BOLD("Bold", Font.BOLD),
-        ITALIC("Italic", Font.ITALIC),
-        PLAIN("Plain", Font.PLAIN);
+public interface DropPartyConfig extends Config
+{
+	@ConfigItem(
+			keyName = "playerName",
+			name = "Dropping player:",
+			description = "selects what players name to mark tiles",
+			position = 0
+	)
+	default String playerName()
+	{
+		return "";
+	}
 
-        private String name;
-        private int font;
+	@Range(
+			min = 1,
+			max = 40
+	)
+	@ConfigItem(
+			keyName = "showAmmount",
+			name = "Trail length",
+			description = "Shows the legnth of the droppers trail",
+			position = 1
+	)
+	default int showAmmount()
+	{
+		return 10;
+	}
 
-        @Override
-        public String toString()
-        {
-            return getName();
-        }
-    }
+	@ConfigItem(
+			position = 2,
+			keyName = "overlayColor",
+			name = "Overlay Color",
+			description = "Configures the color of the overlay"
+	)
+	default Color overlayColor()
+	{
+		return new Color(0, 150, 200);
+	}
 
-    @ConfigItem(
-            keyName = "playerName",
-            name = "Dropping player:",
-            description = "selects what players name to mark tiles",
-            position = 0
-    )
-    default String playerName()
-    {
-        return "";
-    }
-    @Range(
-            min = 1,
-            max = 40
-    )
-    @ConfigItem(
-            keyName = "showAmmount",
-            name = "trail length",
-            description = "Shows the legnth of the droppers trail",
-            position = 1
-    )
-    default int showAmmount() { return 10; }
+	@ConfigItem(
+			position = 3,
+			keyName = "fontStyle",
+			name = "Font Style",
+			description = "Bold/Italics/Plain"
+	)
+	default FontStyle fontStyle()
+	{
+		return FontStyle.BOLD;
+	}
 
-    @ConfigItem(
-            position = 2,
-            keyName = "overlayColor",
-            name = "Overlay Color",
-            description = "Configures the color of the overlay"
-    )
-    default Color overlayColor()
-    {
-        return new Color(0, 150, 200);
-    }
+	@Range(
+			min = 10,
+			max = 40
+	)
+	@ConfigItem(
+			position = 4,
+			keyName = "textSize",
+			name = "Text Size",
+			description = "Text Size for Timers."
+	)
+	default int textSize()
+	{
+		return 18;
+	}
 
-    @ConfigItem(
-            position = 3,
-            keyName = "fontStyle",
-            name = "Font Style",
-            description = "Bold/Italics/Plain"
-    )
-    default FontStyle fontStyle()
-    {
-        return FontStyle.BOLD;
-    }
+	@Getter
+	@AllArgsConstructor
+	enum FontStyle
+	{
+		BOLD("Bold", Font.BOLD),
+		ITALIC("Italic", Font.ITALIC),
+		PLAIN("Plain", Font.PLAIN);
 
-    @Range(
-            min = 10,
-            max = 40
-    )
-    @ConfigItem(
-            position = 4,
-            keyName = "textSize",
-            name = "Text Size",
-            description = "Text Size for Timers."
-    )
-    default int textSize()
-    {
-        return 18;
-    }
+		private String name;
+		private int font;
+
+		@Override
+		public String toString()
+		{
+			return getName();
+		}
+	}
 
 
 }
--- runelite-client/src/main/java/net/runelite/client/plugins/dropparty/DropPartyPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/dropparty/DropPartyPlugin.java
@@ -1,26 +1,41 @@
+/*
+ * Copyright (c) 2017, Adam <Adam@sigterm.info>
+ * All rights reserved.
+ *
+ *
+ * Modified by farhan1666
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package net.runelite.client.plugins.dropparty;
 
 import com.google.inject.Provides;
-import java.awt.Color;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import javax.inject.Inject;
-import javax.inject.Singleton;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.*;
+import net.runelite.api.Client;
+import net.runelite.api.Player;
 import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.events.ConfigChanged;
-import net.runelite.api.events.GameObjectDespawned;
-import net.runelite.api.events.GameObjectSpawned;
-import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.GameTick;
-import net.runelite.api.events.ProjectileMoved;
-import net.runelite.api.events.ProjectileSpawned;
 import net.runelite.client.Notifier;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.EventBus;
@@ -29,146 +44,168 @@ import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.plugins.PluginType;
 import net.runelite.client.ui.overlay.OverlayManager;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @PluginDescriptor(
-        name = "Drop Party",
-        description = "Marks where a user ran, for drop partys",
-        tags = {"Drop", "Party", "marker", "player"},
-        type = PluginType.UTILITY,
-        enabledByDefault = false
+		name = "Drop Party",
+		description = "Marks where a user ran, for drop partys",
+		tags = {"Drop", "Party", "marker", "player"},
+		type = PluginType.UTILITY,
+		enabledByDefault = false
 )
 @Singleton
 @Slf4j
-public class DropPartyPlugin extends Plugin {
-    @Inject
-    public DropPartyConfig config;
-    @Inject
-    private Notifier notifier;
-    @Inject
-    private OverlayManager overlayManager;
-    @Inject
-    private DropPartyOverlay coreOverlay;
-    @Inject
-    private EventBus eventbus;
-    @Inject
-    private Client client;
-    @Provides
-    DropPartyConfig getConfig(ConfigManager configManager)
-    {
-        return configManager.getConfig(DropPartyConfig.class);
-    }
-    @Getter(AccessLevel.PACKAGE)
-    List<WorldPoint> playerPath = new ArrayList<>();
-    @Getter(AccessLevel.PACKAGE)
-    String playerName = "";
-    @Getter(AccessLevel.PACKAGE)
-    int showAmmount = 0;
-    @Getter(AccessLevel.PACKAGE)
-    int MAXPATHSIZE = 100;
-    Player runningPlayer;
-    @Getter(AccessLevel.PACKAGE)
-    Color overlayColor;
-    @Getter(AccessLevel.PACKAGE)
-    private int fontStyle;
-    @Getter(AccessLevel.PACKAGE)
-    private int textSize;
+public class DropPartyPlugin extends Plugin
+{
+	@Inject
+	public DropPartyConfig config;
+	@Getter(AccessLevel.PACKAGE)
+	List<WorldPoint> playerPath = new ArrayList<>();
+	@Getter(AccessLevel.PACKAGE)
+	String playerName = "";
+	@Getter(AccessLevel.PACKAGE)
+	int showAmmount = 0;
+	@Getter(AccessLevel.PACKAGE)
+	int MAXPATHSIZE = 100;
+	Player runningPlayer;
+	@Getter(AccessLevel.PACKAGE)
+	Color overlayColor;
 
-    @Override
-    protected void startUp()
-    {
-        updateConfig();
-        addSubscriptions();
-        overlayManager.add(coreOverlay);
-        reset();
-    }
+	@Inject
+	private Notifier notifier;
+	@Inject
+	private OverlayManager overlayManager;
+	@Inject
+	private DropPartyOverlay coreOverlay;
+	@Inject
+	private EventBus eventbus;
+	@Inject
+	private Client client;
+	@Getter(AccessLevel.PACKAGE)
+	private int fontStyle;
+	@Getter(AccessLevel.PACKAGE)
+	private int textSize;
 
-    @Override
-    protected void shutDown()
-    {
-        overlayManager.remove(coreOverlay);
-        reset();
-        eventbus.unregister(this);
-    }
+	@Provides
+	DropPartyConfig getConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(DropPartyConfig.class);
+	}
 
-    private void addSubscriptions()
-    {
-        eventbus.subscribe(ConfigChanged.class, this, this::onConfigChanged);
-        eventbus.subscribe(GameTick.class, this, this::onGameTick);
-    }
+	@Override
+	protected void startUp()
+	{
+		updateConfig();
+		addSubscriptions();
+		overlayManager.add(coreOverlay);
+		reset();
+	}
 
+	@Override
+	protected void shutDown()
+	{
+		overlayManager.remove(coreOverlay);
+		reset();
+		eventbus.unregister(this);
+	}
 
-
-    private void onGameTick(GameTick event) {
-        shuffleList();
-        if(playerName.equalsIgnoreCase("") || playerName == null) {
-            return;
-        }
-
-        runningPlayer = null;
-
-        for (Player player : client.getPlayers())
-        {
-            if(player.getName() != null) {
-                if (player.getName().equalsIgnoreCase(playerName))
-                {
-                    runningPlayer = player;
-                    break;
-                }
-            }
-
-        }
-
-        if(runningPlayer == null) {
-            cordsError();
-            return;
-        }
-        addCords();
-    }
-
-    private void cordsError(){
-        playerPath.add(null);
-
-    }
-    private void shuffleList(){
-        if (playerPath.size() > MAXPATHSIZE - 1) {
-            playerPath.remove(0);
-        }
-    }
-
-    private void addCords() {
-        while(true) {
-            if (playerPath.size() >= MAXPATHSIZE) {
-                playerPath.add(runningPlayer.getWorldLocation());
-                break;
-            }
-            playerPath.add(null);
-
-        }
+	private void addSubscriptions()
+	{
+		eventbus.subscribe(ConfigChanged.class, this, this::onConfigChanged);
+		eventbus.subscribe(GameTick.class, this, this::onGameTick);
+	}
 
 
-    }
+	private void onGameTick(GameTick event)
+	{
+		shuffleList();
+		if (playerName.equalsIgnoreCase("") || playerName == null)
+		{
+			return;
+		}
 
-    private void onConfigChanged(ConfigChanged event)
-    {
-        if (!event.getGroup().equals("drop"))
-        {
-            return;
-        }
+		runningPlayer = null;
 
-        updateConfig();
-    }
+		for (Player player : client.getPlayers())
+		{
+			if (player.getName() != null)
+			{
+				if (player.getName().equalsIgnoreCase(playerName))
+				{
+					runningPlayer = player;
+					break;
+				}
+			}
+
+		}
+
+		if (runningPlayer == null)
+		{
+			cordsError();
+			return;
+		}
+		addCords();
+	}
+
+	private void cordsError()
+	{
+		playerPath.add(null);
+
+	}
+
+	private void shuffleList()
+	{
+		if (playerPath.size() > MAXPATHSIZE - 1)
+		{
+			playerPath.remove(0);
+		}
+	}
+
+	private void addCords()
+	{
+		while (true)
+		{
+			if (playerPath.size() >= MAXPATHSIZE)
+			{
+				playerPath.add(runningPlayer.getWorldLocation());
+				break;
+			}
+			playerPath.add(null);
+
+		}
 
 
-    private void reset(){
-        playerPath.clear();
-    }
+	}
+
+	private void onConfigChanged(ConfigChanged event)
+	{
+		if (!event.getGroup().equals("drop"))
+		{
+			return;
+		}
+
+		updateConfig();
+	}
 
 
-    private void updateConfig(){
-        this.playerName = config.playerName();
-        this.showAmmount = config.showAmmount();
-        this.overlayColor = config.overlayColor();
-        this.fontStyle = config.fontStyle().getFont();
-        this.textSize = config.textSize();
-    }
+	private void reset()
+	{
+		playerPath.clear();
+
+	}
+
+
+	private void updateConfig()
+	{
+		this.playerName = config.playerName();
+		this.showAmmount = config.showAmmount();
+		this.overlayColor = config.overlayColor();
+		this.fontStyle = config.fontStyle().getFont();
+		this.textSize = config.textSize();
+	}
 }
