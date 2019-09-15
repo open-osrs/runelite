0ab6b262f45b840f08590fbd455bf7b1ac2da74d
Ian William O'Neill <iano2k4@hotmail.com>
2019-09-15 23:08:54 +0100

    sulliusceps: removed.

--- runelite-client/src/main/java/net/runelite/client/plugins/sulliusceps/SulliuscepsConfig.java
+++
@@ -1,9 +0,0 @@
-package net.runelite.client.plugins.sulliusceps;
-
-import net.runelite.client.config.Config;
-import net.runelite.client.config.ConfigGroup;
-
-@ConfigGroup("motherlode")
-public interface SulliuscepsConfig extends Config
-{
-}
