55c993b5abc71cacde03f7474cad5c05100f66d0
Crystalknoct <andrewj0013@gmail.com>
2019-09-25 17:10:11 -0600

    itemchargeoverlay: yes
    123

--- runelite-client/src/main/java/net/runelite/client/plugins/itemcharges/ItemChargeOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/itemcharges/ItemChargeOverlay.java
@@ -39,6 +39,8 @@ import static net.runelite.client.plugins.itemcharges.ItemChargeType.IMPBOX;
 import static net.runelite.client.plugins.itemcharges.ItemChargeType.TELEPORT;
 import static net.runelite.client.plugins.itemcharges.ItemChargeType.WATERCAN;
 import static net.runelite.client.plugins.itemcharges.ItemChargeType.WATERSKIN;
+import static net.runelite.client.plugins.itemcharges.ItemChargeType.FRUIT_BASKET;
+import static net.runelite.client.plugins.itemcharges.ItemChargeType.SACK;
 import net.runelite.client.ui.FontManager;
 import net.runelite.client.ui.overlay.WidgetItemOverlay;
 import net.runelite.client.ui.overlay.components.TextComponent;
@@ -160,6 +162,8 @@ class ItemChargeOverlay extends WidgetItemOverlay
 				|| (type == WATERCAN && !plugin.isShowWateringCanCharges())
 				|| (type == WATERSKIN && !plugin.isShowWaterskinCharges())
 				|| (type == BELLOWS && !plugin.isShowBellowCharges())
+				|| (type == FRUIT_BASKET && !plugin.isShowBasketCharges())
+				|| (type == SACK && !plugin.isShowSackCharges())
 				|| (type == ABYSSAL_BRACELET && !plugin.isShowAbyssalBraceletCharges()))
 			{
 				return;
@@ -181,6 +185,6 @@ class ItemChargeOverlay extends WidgetItemOverlay
 		return plugin.isShowTeleportCharges() || plugin.isShowDodgyCount() || plugin.isShowFungicideCharges()
 			|| plugin.isShowImpCharges() || plugin.isShowWateringCanCharges() || plugin.isShowWaterskinCharges()
 			|| plugin.isShowBellowCharges() || plugin.isShowAbyssalBraceletCharges() || plugin.isShowExplorerRingCharges()
-			|| plugin.isShowRingOfForgingCount();
+			|| plugin.isShowRingOfForgingCount() || plugin.isShowBasketCharges() || plugin.isShowSackCharges();
 	}
 }
