1528d30e06b1270a562d103c0db4f52e157c776a
Owain van Brakel <owain.vanbrakel@gmail.com>
2019-09-28 17:49:08 +0200

    checkstyle

--- injector-plugin/src/main/java/net/runelite/injector/Inject.java
+++ injector-plugin/src/main/java/net/runelite/injector/Inject.java
@@ -46,14 +46,19 @@ import net.runelite.asm.signature.Signature;
 import net.runelite.deob.DeobAnnotations;
 import net.runelite.deob.deobfuscators.arithmetic.DMath;
 import static net.runelite.injector.InjectUtil.getFieldType;
-
-import net.runelite.injector.raw.*;
-// import net.runelite.injector.raw.DrawMenu;
-// import net.runelite.injector.raw.HidePlayerAttacks;
+import net.runelite.injector.raw.ClearColorBuffer;
+import net.runelite.injector.raw.DrawAfterWidgets;
+import net.runelite.injector.raw.Occluder;
+import net.runelite.injector.raw.RasterizerHook;
+import net.runelite.injector.raw.RenderDraw;
+import net.runelite.injector.raw.ScriptVM;
 import net.runelite.mapping.Import;
 import net.runelite.rs.api.RSClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+// import net.runelite.injector.raw.DrawMenu;
+// import net.runelite.injector.raw.HidePlayerAttacks;
 
 public class Inject
 {
