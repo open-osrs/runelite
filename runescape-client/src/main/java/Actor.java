dc22e56b87fb8589fc17ab096be8085eb0dff1f7
chaos50006 <benjamin.carr3@gmail.com>
2019-10-03 05:21:35 +0100

    Revert "project: update from 184 to 184 (#1675)"
    This reverts commit cb57ab26b32eeaef0871d6c115f7e38dc4ff294c.
    
    # Conflicts:
    #	injector-plugin/src/main/java/net/runelite/injector/Inject.java

--- runescape-client/src/main/java/Actor.java
+++ runescape-client/src/main/java/Actor.java
@@ -14,9 +14,6 @@ public abstract class Actor extends Entity {
 	@Export("x")
 	int x;
 	@ObfuscatedName("ap")
-	@ObfuscatedGetter(
-		intValue = 682054857
-	)
 	@Export("y")
 	int y;
 	@ObfuscatedName("ar")
@@ -377,7 +374,7 @@ public abstract class Actor extends Entity {
 		signature = "(B)V",
 		garbageValue = "4"
 	)
-	final void method1765() {
+	final void method1761() {
 		this.pathLength = 0;
 		this.field988 = 0;
 	}
