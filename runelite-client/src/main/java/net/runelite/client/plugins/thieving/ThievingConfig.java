5ec4be22dbb844f9ff3f225205cf515910438105
Manatsawin Hanmongkolchai <manatsawin@wongnai.com>
2019-10-29 06:49:37 +0700

    Rearrange config

--- runelite-client/src/main/java/net/runelite/client/plugins/thieving/ThievingConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/thieving/ThievingConfig.java
@@ -37,6 +37,18 @@ import net.runelite.client.config.Range;
 @ConfigGroup("thieving")
 public interface ThievingConfig extends Config
 {
+	@ConfigItem
+		(
+			position = 1,
+			keyName = "statTimeout",
+			name = "Reset stats (minutes)",
+			description = "Change the time until the thieving session is reset and the overlay is hidden"
+		)
+	default int statTimeout()
+	{
+		return 5;
+	}
+
 	@ConfigSection(
 		name = "Chest",
 		description = "",
@@ -46,18 +58,6 @@ public interface ThievingConfig extends Config
 	default boolean chestSection()
 	{
 		return false;
-	}
-
-	@ConfigItem
-	(
-		position = 1,
-		keyName = "statTimeout",
-		name = "Reset stats (minutes)",
-		description = "Change the time until the thieving session is reset and the overlay is hidden"
-	)
-	default int statTimeout()
-	{
-		return 5;
 	}
 
 	@Alpha
