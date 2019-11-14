f6764ff1ad1b907e8aca157131130347d2457da2
f0rmatme <jmunson@openpoll.io>
2019-11-14 15:56:25 -0800

    rebase

--- runelite-client/src/main/java/net/runelite/client/plugins/specialcounter/Boss.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/specialcounter/Boss.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2018, Raqes <j.raqes@gmail.com>
- * All rights reserved.
+ * All rights reserved
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
