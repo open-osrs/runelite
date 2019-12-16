f53d0e96235a3d8db26c586f53e3f0335fe6374f
Dylan White <Whitedylan7@gmail.com>
2019-12-15 23:30:13 -0600

    Forgot a whitespace in itemtype

--- runelite-client/src/main/java/net/runelite/client/plugins/suppliestracker/ItemType.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/suppliestracker/ItemType.java
@@ -87,7 +87,7 @@ public enum ItemType
 		{
 			return ItemType.JEWELLERY;
 		}
-		if (item.getId() == SCYTHE_OF_VITUR || item.getId() == SANGUINESTI_STAFF||
+		if (item.getId() == SCYTHE_OF_VITUR || item.getId() == SANGUINESTI_STAFF ||
 				item.getId() == TRIDENT_OF_THE_SEAS || item.getId() == TRIDENT_OF_THE_SWAMP)
 		{
 			return ItemType.CHARGES;
