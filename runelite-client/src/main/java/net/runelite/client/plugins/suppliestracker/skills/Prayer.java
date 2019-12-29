77735c89745c325dd138b30c91992bf6e5c1a625
Dylan White <Whitedylan7@gmail.com>
2019-12-29 17:21:22 -0600

    rename Skills to skills

--- runelite-client/src/main/java/net/runelite/client/plugins/suppliestracker/skills/Prayer.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/suppliestracker/skills/Prayer.java
@@ -1,4 +1,4 @@
-package net.runelite.client.plugins.suppliestracker.Skills;
+package net.runelite.client.plugins.suppliestracker.skills;
 
 import net.runelite.client.game.ItemManager;
 import net.runelite.client.plugins.suppliestracker.SuppliesTrackerPlugin;
