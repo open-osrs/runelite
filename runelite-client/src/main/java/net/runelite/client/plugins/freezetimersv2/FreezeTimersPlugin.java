a5d3983a94a95a245329ffe0f3cc043d9c070946
ThatGamerBlue <thatgamerblue@gmail.com>
2020-01-19 14:04:37 +0000

    start work on v2

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/freezetimersv2/FreezeTimersPlugin.java
@@ -0,0 +1,53 @@
+package net.runelite.client.plugins.freezetimersv2;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import net.runelite.api.events.GameTick;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.plugins.PluginType;
+import net.runelite.client.ui.overlay.OverlayManager;
+
+import javax.inject.Inject;
+
+@PluginDescriptor(
+	name = "Freeze Timers v2",
+	description = "Freeze/teleblock/veng timer overlay on players",
+	tags = {"freeze", "timers", "barrage", "teleblock", "pklite"},
+	type = PluginType.PVP,
+	enabledByDefault = false
+)
+public class FreezeTimersPlugin extends Plugin
+{
+	@Inject
+	private OverlayManager overlayManager;
+
+	@Inject
+	private PrayerTracker prayerTracker;
+
+	@Getter(AccessLevel.PACKAGE)
+	@Inject
+	private TimerManager timerManager;
+
+	@Inject
+	private FreezeTimersOverlay overlay;
+
+	@Override
+	public void startUp()
+	{
+		overlayManager.add(overlay);
+	}
+
+	@Override
+	public void shutDown()
+	{
+		overlayManager.remove(overlay);
+	}
+
+	@Subscribe
+	public void onGameTick(GameTick gameTick)
+	{
+		prayerTracker.gameTick();
+	}
+}
---
+++ runelite-client/src/main/java/net/runelite/client/plugins/freezetimersv2/Timer.java
@@ -0,0 +1,71 @@
+package net.runelite.client.plugins.freezetimersv2;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Value;
+import lombok.experimental.NonFinal;
+import net.runelite.api.Client;
+
+import java.awt.image.BufferedImage;
+
+@Value
+@Getter(AccessLevel.NONE)
+public class Timer
+{
+	private Client client;
+	private int ticksStart;
+	private int ticksLength;
+	@NonFinal
+	private TimerType type;
+	@NonFinal
+	private boolean shutdown = false;
+
+	public Timer(Client client, int length, TimerType type)
+	{
+		this.client = client;
+		this.ticksStart = client.getTickCount();
+		this.ticksLength = length;
+		this.type = type;
+	}
+
+	public void setTimerTypeIfNull(TimerType set)
+	{
+		if (type == null)
+		{
+			type = set;
+		}
+	}
+
+	private int ticksRemaining()
+	{
+		int ticksElapsed = client.getTickCount() - ticksStart;
+
+		return ticksLength - ticksElapsed;
+	}
+
+	public boolean isActive()
+	{
+		return ticksRemaining() > 0;
+	}
+
+	public boolean isInCooldown()
+	{
+		if (!isActive())
+		{
+			return false;
+		}
+
+		return ticksRemaining() < type.getImmunityLength();
+	}
+
+	public int ticksRemainingForDisplay()
+	{
+		return ticksRemaining() - type.getImmunityLength(); // this will cause all immunities to be negative, making it easier to render later
+	}
+
+	public BufferedImage getIcon()
+	{
+		return isInCooldown() ? type.getCooldownIcon() : type.getIcon();
+	}
+
+}
