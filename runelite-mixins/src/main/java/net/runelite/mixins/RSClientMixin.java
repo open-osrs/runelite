81542d02a6d6681f089fe0319d73a168c19b1441
Lucwousin <Lucwousin@users.noreply.github.com>
2020-06-27 15:09:15 +0200

    Bring player targeted by hint arrow to front (while not in combat)

--- runelite-mixins/src/main/java/net/runelite/mixins/RSClientMixin.java
+++ runelite-mixins/src/main/java/net/runelite/mixins/RSClientMixin.java
@@ -1196,6 +1196,18 @@ public abstract class RSClientMixin implements RSClient
 		client.getCallbacks().post(CanvasSizeChanged.class, CanvasSizeChanged.INSTANCE);
 	}
 
+	@FieldHook("hintArrowPlayerIndex")
+	@Inject
+	public static void hintPlayerChanged(int ignored)
+	{
+		// Setting the localInteractingIndex (aka player target index, it only applies to players)
+		// causes that player to get priority over others when rendering/menus are added
+		if (client.getVar(VarPlayer.ATTACKING_PLAYER) == -1)
+		{
+			client.setLocalInteractingIndex(client.getHintArrowPlayerTargetIdx() & 2047);
+		}
+	}
+
 	@Inject
 	@Override
 	public boolean hasHintArrow()
@@ -1247,6 +1259,7 @@ public abstract class RSClientMixin implements RSClient
 	{
 		client.setHintArrowTargetType(HintArrowType.PLAYER.getValue());
 		client.setHintArrowPlayerTargetIdx(((RSPlayer) player).getPlayerId());
+		hintPlayerChanged(-1);
 	}
 
 	@Inject
