4a737e9fab10a82b82f84d9cf6c0408fe7987a10
Noodleeater <somawheels@gmail.com>
2020-11-22 23:07:35 +0000

    Added some descriptions

--- runelite-api/src/main/java/net/runelite/api/Skeleton.java
+++ runelite-api/src/main/java/net/runelite/api/Skeleton.java
@@ -1,10 +1,30 @@
 package net.runelite.api;
 
+/**
+ * Represents a Skeleton.
+ * When calculating a model's pose in a animation frame, the skeleton is used to link vertices to animation values.
+ */
 public interface Skeleton extends Node
 {
+	/**
+	 * the number of bones in the Skeleton
+	 */
 	int getCount();
 
+	/**
+	 * the transform type of each bone
+	 * The possible transform types are as follows:
+	 * 0 (set pivot point)
+	 * 1 (translate)
+	 * 2 (rotate)
+	 * 3 (scale)
+	 * 5 (alpha/transparency)
+	 */
 	int[] getTypes();
 
+	/**
+	 * Each bone is linked to a list of vertex skins. [bone id][list of vertex skins]
+	 * if a vertex's skin is linked to a bone, the vertex can be transformed by that bone.
+	 */
 	int[][] getList();
 }
