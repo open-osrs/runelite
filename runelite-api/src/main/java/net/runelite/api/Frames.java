b76b33b40e8e2cea5cce4052ea1727d588f0dfb2
Noodleeater <somawheels@gmail.com>
2020-11-22 20:26:44 +0000

    added frames.java to runelite api

---
+++ runelite-api/src/main/java/net/runelite/api/Frames.java
@@ -0,0 +1,7 @@
+package net.runelite.api;
+
+import net.runelite.api.Animation;
+public interface Frames extends DualNode
+{
+    Animation[] getFrames();
+}
--- runescape-api/src/main/java/net/runelite/rs/api/RSFrames.java
+++ runescape-api/src/main/java/net/runelite/rs/api/RSFrames.java
@@ -24,9 +24,11 @@
  */
 package net.runelite.rs.api;
 
+import net.runelite.api.Animation;
+import net.runelite.api.Frames;
 import net.runelite.mapping.Import;
 
-public interface RSFrames extends RSDualNode
+public interface RSFrames extends RSDualNode, Frames
 {
 	@Import("frames")
 	RSAnimation[] getFrames();
