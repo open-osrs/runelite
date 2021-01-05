8169d144d68f2c04bc0b2cdba7fb29d7670a69e0
ThatGamerBlue <thatgamerblue@gmail.com>
2021-01-05 11:31:48 +0000

    thank you mr polar <3

--- runelite-mixins/src/main/java/net/runelite/mixins/RSGameShellMixin.java
+++ runelite-mixins/src/main/java/net/runelite/mixins/RSGameShellMixin.java
@@ -27,6 +27,7 @@ package net.runelite.mixins;
 import net.runelite.api.events.FocusChanged;
 import net.runelite.api.hooks.DrawCallbacks;
 import java.awt.event.FocusEvent;
+import net.runelite.api.mixins.Copy;
 import net.runelite.api.mixins.FieldHook;
 import net.runelite.api.mixins.Inject;
 import net.runelite.api.mixins.MethodHook;
@@ -107,5 +108,19 @@ public abstract class RSGameShellMixin implements RSGameShell
 	{
 		//Always allow host.
 		return true;
+	}
+
+	@Copy("replaceCanvas")
+	@Replace("replaceCanvas")
+	@SuppressWarnings("InfiniteRecursion")
+	public void copy$replaceCanvas()
+	{
+		if (client != null && client.isGpu())
+		{
+			setFullRedraw(false);
+			return;
+		}
+
+		copy$replaceCanvas();
 	}
 }
