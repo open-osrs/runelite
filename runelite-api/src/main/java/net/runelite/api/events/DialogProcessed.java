ab46bcabe4678854832986ba9baa0c342579d6a8
Burak <burakinan1@hotmail.com>
2021-01-13 21:14:03 +0100

    Add DialogProcessed event

---
+++ runelite-api/src/main/java/net/runelite/api/events/DialogProcessed.java
@@ -0,0 +1,10 @@
+package net.runelite.api.events;
+
+import lombok.Value;
+import net.runelite.api.DialogOption;
+
+@Value
+public class DialogProcessed implements Event
+{
+	DialogOption dialogOption;
+}
