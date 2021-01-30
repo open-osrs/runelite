e678c83ff987c589ec56e070d7a2cb221de90573
Noodleeater <somawheels@gmail.com>
2021-01-30 20:37:02 +0000

    added licence headers

--- runescape-api/src/main/java/net/runelite/rs/api/RSAbstractArchive.java
+++ runescape-api/src/main/java/net/runelite/rs/api/RSAbstractArchive.java
@@ -1,9 +1,10 @@
 package net.runelite.rs.api;
 
+import net.runelite.api.AbstractArchive;
 import net.runelite.api.IndexDataBase;
 import net.runelite.mapping.Import;
 
-public interface RSAbstractArchive extends IndexDataBase
+public interface RSAbstractArchive extends IndexDataBase, AbstractArchive
 {
 	@Import("takeFile")
 	byte[] getConfigData(int archiveId, int fileId);
@@ -11,4 +12,20 @@ public interface RSAbstractArchive extends IndexDataBase
 	@Import("getGroupFileIds")
 	@Override
 	int[] getFileIds(int group);
+
+	@Import("groupCount")
+	int getGroupCount();
+
+	@Import("fileIds")
+	int[][] getFileIds();
+
+	@Import("getFile")
+	byte[] getFile(int var1, int var2);
+
+	@Import("getGroupFileCount")
+
+	int getGroupFileCount(int var1);
+
+	@Import("fileCounts")
+	int[] getFileCounts();
 }
