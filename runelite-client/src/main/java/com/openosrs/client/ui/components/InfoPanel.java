51301a274f9eafbf715affdefdd1a8bb5a227b44
ThatGamerBlue <thatgamerblue@gmail.com>
2021-02-21 16:22:43 +0000

    .runelite -> .openosrs

--- runelite-client/src/main/java/com/openosrs/client/ui/components/InfoPanel.java
+++ runelite-client/src/main/java/com/openosrs/client/ui/components/InfoPanel.java
@@ -57,7 +57,7 @@ public class InfoPanel extends JPanel
 	private static final BufferedImage TRANSPARENT_LOGO = ImageUtil.getResourceStreamFromClass(InfoPanel.class, "/openosrs.png");
 	static final Dimension PANEL_SIZE = new Dimension(200, OpenOSRSSplashScreen.FRAME_SIZE.height);
 	private static final Dimension VERSION_SIZE = new Dimension(PANEL_SIZE.width, 25);
-	private static final File RUNELITE_DIR = new File(System.getProperty("user.home"), ".runelite");
+	private static final File RUNELITE_DIR = new File(System.getProperty("user.home"), ".openosrs");
 	private static final File LOGS_DIR = new File(RUNELITE_DIR, "logs");
 
 	public InfoPanel()
