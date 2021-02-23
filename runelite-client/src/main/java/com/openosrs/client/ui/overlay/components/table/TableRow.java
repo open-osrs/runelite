d390a516dd98cf0d3d357b943a45a525e8cd08c8
Owain van Brakel <owain.vanbrakel@gmail.com>
2021-02-23 05:37:09 +0100

    tablecomponent: Fix package

--- runelite-client/src/main/java/com/openosrs/client/ui/overlay/components/table/TableRow.java
+++ runelite-client/src/main/java/com/openosrs/client/ui/overlay/components/table/TableRow.java
@@ -22,7 +22,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package net.runelite.client.ui.overlay.components.table;
+package com.openosrs.client.ui.overlay.components.table;
 
 import java.awt.Color;
 import java.util.Collections;
