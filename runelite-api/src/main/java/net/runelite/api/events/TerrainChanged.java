7270d61392c102c925b950de141d2d31f6d53bf1
IamZoY <71918831+IamZoY@users.noreply.github.com>
2021-02-25 02:03:32 +0300

    removed iface

--- runelite-api/src/main/java/net/runelite/api/events/TerrainChanged.java
+++ runelite-api/src/main/java/net/runelite/api/events/TerrainChanged.java
@@ -1,9 +1,11 @@
 package net.runelite.api.events;
 
 public class TerrainChanged
-		implements Event {
+{
 	public static final TerrainChanged INSTANCE = new TerrainChanged();
 
-	private TerrainChanged() {
+	private TerrainChanged()
+	{
+
 	}
 }
