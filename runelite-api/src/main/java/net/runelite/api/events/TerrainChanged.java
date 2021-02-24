01423ee97ee99821dce252fff8a33b9ca406454f
IamZoY <71918831+IamZoY@users.noreply.github.com>
2021-02-25 01:54:47 +0300

    Added TerrainChanged

---
+++ runelite-api/src/main/java/net/runelite/api/events/TerrainChanged.java
@@ -0,0 +1,9 @@
+package net.runelite.api.events;
+
+public class TerrainChanged
+		implements Event {
+	public static final TerrainChanged INSTANCE = new TerrainChanged();
+
+	private TerrainChanged() {
+	}
+}
