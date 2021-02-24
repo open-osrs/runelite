11dd1cdb415fd2e9ce43101fdfa6db7ee6ae125b
zeruth <therealnull@gmail.com>
2021-02-24 00:10:32 -0500

    chore: bump rl ver

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -25,7 +25,7 @@
 
 object ProjectVersions {
     const val launcherVersion = "2.2.0"
-    const val rlVersion = "1.6.38"
+    const val rlVersion = "1.7.0"
 
     const val openosrsVersion = "4.0.0"
 
