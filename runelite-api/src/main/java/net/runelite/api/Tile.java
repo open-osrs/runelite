4da926cad59a67cabfa9d06b4f00a0372d647d34
ImNo <kylian@iphone.nl>
2021-03-06 15:43:35 +0100

    Revert "Update Tile.java"
    This reverts commit 8a5fffc34f5a8ad0ae781da084adc2254ec39067.

--- runelite-api/src/main/java/net/runelite/api/Tile.java
+++ runelite-api/src/main/java/net/runelite/api/Tile.java
@@ -39,13 +39,6 @@ public interface Tile extends TileObject
 	DecorativeObject getDecorativeObject();
 
 	/**
-	 * Sets the object on the ground layer of the tile.
-	 *
-	 * @param decorativeObject the ground object
-	 */
-	void setDecorativeObject(DecorativeObject decorativeObject);
-
-	/**
 	 * Gets all game objects on the tile.
 	 *
 	 * @return the game objects
