257103dbcd075f77f2f3f38aceae7b2ecc62ba24
Jonatino <jonathanbeaudoin1996@gmail.com>
2021-03-25 18:19:48 -0400

    Remove more static imports

--- runescape-api/src/main/java/net/runelite/rs/api/RSMinimapRenderer.java
+++ runescape-api/src/main/java/net/runelite/rs/api/RSMinimapRenderer.java
@@ -1,12 +1,3 @@
 package net.runelite.rs.api;
 
-import net.runelite.mapping.Import;
-
-public interface RSMinimapRenderer
-{
-	@Import("cacheParentPaths")
-	String[] getCacheParentPaths();
-	
-	@Import("cacheParentPaths")
-	void setCacheParentPaths(String[] cacheParentPaths);
-}
+public interface RSMinimapRenderer {}
