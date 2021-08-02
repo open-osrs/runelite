e60a41c355f4fc5b94a0b7bf20af9b3f4c97fc53
Owain van Brakel <owain.vanbrakel@gmail.com>
2021-08-02 12:18:30 +0200

    mixins: Checkstyle

--- runelite-mixins/src/main/java/net/runelite/mixins/RSArchiveMixin.java
+++ runelite-mixins/src/main/java/net/runelite/mixins/RSArchiveMixin.java
@@ -9,19 +9,22 @@ import net.runelite.rs.api.RSArchiveDisk;
 import net.runelite.rs.api.RSClient;
 
 @Mixin(RSArchive.class)
-public abstract class RSArchiveMixin implements RSArchive {
+public abstract class RSArchiveMixin implements RSArchive
+{
 
-    @Shadow("client")
-    private static RSClient client;
+	@Shadow("client")
+	private static RSClient client;
 
-    @Shadow("archives")
-    private static RSArchive[] archives;
+	@Shadow("archives")
+	private static RSArchive[] archives;
 
-    @MethodHook(value = "<init>", end = true)
-    @Inject
-    private void rl$init(RSArchiveDisk var1, RSArchiveDisk var2, int archive, boolean var4, boolean var5, boolean var6) {
-        if (archive >= 0 && archive < 21) {
-            archives[archive] = this;
-        }
-    }
+	@MethodHook(value = "<init>", end = true)
+	@Inject
+	private void rl$init(RSArchiveDisk var1, RSArchiveDisk var2, int archive, boolean var4, boolean var5, boolean var6)
+	{
+		if (archive >= 0 && archive < 21)
+		{
+			archives[archive] = this;
+		}
+	}
 }
