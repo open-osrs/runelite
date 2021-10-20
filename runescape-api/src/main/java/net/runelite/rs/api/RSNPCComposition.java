5c19e731fad8a11f45e377b238a569c7703b7c30
Owain van Brakel <owain.vanbrakel@gmail.com>
2021-10-20 16:03:16 +0200

    project: Add workaround for now

--- runescape-api/src/main/java/net/runelite/rs/api/RSNPCComposition.java
+++ runescape-api/src/main/java/net/runelite/rs/api/RSNPCComposition.java
@@ -1,5 +1,6 @@
 package net.runelite.rs.api;
 
+import net.runelite.api.IterableHashTable;
 import net.runelite.api.NPCComposition;
 import net.runelite.mapping.Import;
 
@@ -62,6 +63,9 @@ public interface RSNPCComposition extends NPCComposition
 
 	@Import("params")
 	RSIterableNodeHashTable getParams();
+
+	@Import("params")
+	void setParams(IterableHashTable params);
 
 	@Import("params")
 	void setParams(RSIterableNodeHashTable params);
