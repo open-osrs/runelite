dadcbedcc1a0cbcdff777191d7aa0c8f9aee60a8
Justin <git@justin.pm>
2021-11-07 18:15:28 +1100

    Update IllegalStateExceptionsTest.java
    imports whoops

--- deobfuscator/src/test/java/net/runelite/deob/deobfuscators/IllegalStateExceptionsTest.java
+++ deobfuscator/src/test/java/net/runelite/deob/deobfuscators/IllegalStateExceptionsTest.java
@@ -33,6 +33,7 @@ import net.runelite.deob.TemporyFolderLocation;
 import net.runelite.deob.util.JarUtil;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
