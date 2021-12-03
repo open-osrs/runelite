2f00fcc9e23756c14521efb41c9576d547425d9e
Justin <git@justin.pm>
2021-12-04 10:41:17 +1100

    project: bump version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -27,7 +27,7 @@ object ProjectVersions {
     const val launcherVersion = "2.2.0"
     const val rlVersion = "1.8.5"
 
-    const val openosrsVersion = "4.15.6"
+    const val openosrsVersion = "4.15.7"
 
     const val rsversion = 201
     const val cacheversion = 165
