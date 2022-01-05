ce122c835d3ad3648ef9dc1a2661ffa363077eec
Justin <git@justin.pm>
2022-01-06 10:43:39 +1100

    mixins: add NPC Overheads

--- runelite-mixins/src/main/java/net/runelite/mixins/RSNPCCompositionMixin.java
+++ runelite-mixins/src/main/java/net/runelite/mixins/RSNPCCompositionMixin.java
@@ -5,6 +5,17 @@ import static net.runelite.api.HeadIcon.MAGIC;
 import static net.runelite.api.HeadIcon.MELEE;
 import static net.runelite.api.HeadIcon.RANGED;
 import static net.runelite.api.HeadIcon.RANGE_MAGE;
+import static net.runelite.api.HeadIcon.REDEMPTION;
+import static net.runelite.api.HeadIcon.RETRIBUTION;
+import static net.runelite.api.HeadIcon.SMITE;
+import static net.runelite.api.HeadIcon.RANGE_MELEE;
+import static net.runelite.api.HeadIcon.MAGE_MELEE;
+import static net.runelite.api.HeadIcon.RANGE_MAGE_MELEE;
+import static net.runelite.api.HeadIcon.WRATH;
+import static net.runelite.api.HeadIcon.SOUL_SPLIT;
+import static net.runelite.api.HeadIcon.DEFLECT_MELEE;
+import static net.runelite.api.HeadIcon.DEFLECT_RANGE;
+import static net.runelite.api.HeadIcon.DEFLECT_MAGE;
 import net.runelite.api.events.NpcActionChanged;
 import net.runelite.api.mixins.FieldHook;
 import net.runelite.api.mixins.Inject;
@@ -31,8 +42,30 @@ public abstract class RSNPCCompositionMixin implements RSNPCComposition
 				return RANGED;
 			case 2:
 				return MAGIC;
+			case 3:
+				return RETRIBUTION;
+			case 4:
+				return SMITE;
+			case 5:
+				return REDEMPTION;
 			case 6:
 				return RANGE_MAGE;
+			case 7:
+				return RANGE_MELEE;
+			case 8:
+				return MAGE_MELEE;
+			case 9:
+				return RANGE_MAGE_MELEE;
+			case 10:
+				return WRATH;
+			case 11:
+				return SOUL_SPLIT;
+			case 12:
+				return DEFLECT_MELEE;
+			case 13:
+				return DEFLECT_RANGE;
+			case 14:
+				return DEFLECT_MAGE;
 			default:
 				return null;
 		}
