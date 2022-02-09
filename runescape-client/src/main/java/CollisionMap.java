264cd502816cdfc99b72cca5e57c3395d324c5dd
JumpIfZero <defchinhunter@gmail.com>
2022-02-09 15:33:05 +0200

    rev: 203 fix

--- runescape-client/src/main/java/CollisionMap.java
+++ runescape-client/src/main/java/CollisionMap.java
@@ -247,7 +247,8 @@ public class CollisionMap {
 		descriptor = "(IIB)V",
 		garbageValue = "-55"
 	)
-	public void method3650(int var1, int var2) {
+	@Export("setBlockedByFloor")
+	public void setBlockedByFloor(int var1, int var2) {
 		var1 -= this.xInset; // L: 199
 		var2 -= this.yInset; // L: 200
 		int[] var10000 = this.flags[var1]; // L: 201
--- runescape-client/src/main/java/LoginPacket.java
+++ runescape-client/src/main/java/LoginPacket.java
@@ -51,7 +51,8 @@ public class LoginPacket implements class253 {
 	@ObfuscatedSignature(
 		descriptor = "[Lis;"
 	)
-	static final LoginPacket[] field3070;
+	@Export("LoginPacket_indexedValues")
+	static final LoginPacket[] LoginPacket_indexedValues;
 	@ObfuscatedName("w")
 	@ObfuscatedGetter(
 		intValue = -997966029
@@ -67,12 +68,12 @@ public class LoginPacket implements class253 {
 		field3066 = new LoginPacket(19, -2); // L: 9
 		field3063 = new LoginPacket(26, 8); // L: 10
 		field3068 = new LoginPacket(27, 0); // L: 11
-		field3070 = new LoginPacket[32]; // L: 13
+		LoginPacket_indexedValues = new LoginPacket[32]; // L: 13
 		LoginPacket[] var0 = new LoginPacket[]{field3063, field3066, field3064, field3071, field3068, field3065, field3067}; // L: 18
 		LoginPacket[] var1 = var0; // L: 20
 
 		for (int var2 = 0; var2 < var1.length; ++var2) { // L: 21
-			field3070[var1[var2].id] = var1[var2]; // L: 22
+			LoginPacket_indexedValues[var1[var2].id] = var1[var2]; // L: 22
 		}
 
 	} // L: 24
