cff0f4af24dbaf687671f4203a00592518c7791b
Owain van Brakel <owain.vanbrakel@gmail.com>
2022-02-09 13:25:17 +0100

    project: Bump RuneLite version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -25,7 +25,7 @@
 
 object ProjectVersions {
     const val launcherVersion = "2.2.0"
-    const val rlVersion = "1.8.10"
+    const val rlVersion = "1.8.11"
 
     const val openosrsVersion = "4.20.0"
 
