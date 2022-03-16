4b99e4f5ba99464573738fae06680d22db50ccba
JumpIfZero <defchinhunter@gmail.com>
2022-03-16 15:23:41 +0200

    project: Bump RL version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -25,7 +25,7 @@
 
 object ProjectVersions {
     const val launcherVersion = "2.2.0"
-    const val rlVersion = "1.8.13"
+    const val rlVersion = "1.8.13.1"
 
     const val openosrsVersion = "4.20.3"
 
