5078be94b6cc9196a1621165ee16aac65ac4dfb2
Justin <git@justin.pm>
2022-03-28 12:26:22 +1100

    project: bump RL version
    missing upstream merge + mixins. to be completed at a later date. enables GOTR plugin from plugin-hub

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -25,7 +25,7 @@
 
 object ProjectVersions {
     const val launcherVersion = "2.2.0"
-    const val rlVersion = "1.8.15"
+    const val rlVersion = "1.8.15.1"
 
     const val openosrsVersion = "4.21.0"
 
