1cdb6e906008be1e37ecd3e8d3e929d41cfc39e9
Owain van Brakel <owain.vanbrakel@gmail.com>
2022-04-21 10:43:54 +0200

    project(version): Bump RL version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -25,7 +25,7 @@
 
 object ProjectVersions {
     const val launcherVersion = "2.2.0"
-    const val rlVersion = "1.8.18"
+    const val rlVersion = "1.8.18.1"
 
     const val openosrsVersion = "4.24.1"
 
