8b6aafef68b40c817fc37f23de0f260cd6149f10
Owain van Brakel <owain.vanbrakel@gmail.com>
2022-04-21 10:42:43 +0200

    project(mixins): Always try to grab a widget

--- runelite-mixins/src/main/java/net/runelite/mixins/RuneLiteMenuEntryMixin.java
+++ runelite-mixins/src/main/java/net/runelite/mixins/RuneLiteMenuEntryMixin.java
@@ -65,23 +65,26 @@ public abstract class RuneLiteMenuEntryMixin implements RSRuneLiteMenuEntry
 	@Override
 	public Widget getWidget()
 	{
-		MenuAction menuAction = this.getType();
-		if (menuAction == MenuAction.CC_OP || menuAction == MenuAction.CC_OP_LOW_PRIORITY)
-		{
-			int param1 = this.getParam1();
-			int param0 = this.getParam0();
-			if (param1 == 9764864)
-			{
-				Widget widget = client.getWidget(param1);
-				if (param0 != -1)
-				{
-					widget = widget.getChild(param0);
-				}
+		int param1 = this.getParam1();
+		int param0 = this.getParam0();
 
-				return widget;
+		Widget widget = client.getWidget(param1);
+
+		if (widget == null)
+		{
+			return null;
+		}
+
+		if (param0 != -1)
+		{
+			Widget child = widget.getChild(param0);
+
+			if (child != null)
+			{
+				return child;
 			}
 		}
 
-		return null;
+		return widget;
 	}
 }
